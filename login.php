d83c53072eb05f1316a56a866aa7c9694e732d38
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-09 17:46:32 +0530

    update page

--- login.php
+++ login.php
@@ -9,16 +9,32 @@
 </head>
 <body class="login-body">
     
-<form class="shadow p-4">
+<form method="POST" action="app/login.php" class="shadow p-4">
     <h3 class="display-4">LOGIN</h3>
+    <?php if(isset($_GET['error'])){
+      ?>
+         <div class="alert alert-danger" role="alert">
+      <?php echo stripslashes($_GET['error']);  ?>
+    </div>
+      <?php  } ?>
+
+
+      <?php if(isset($_GET['success'])){
+      ?>
+         <div class="alert alert-success" role="alert">
+      <?php echo stripslashes($_GET['success']);  ?>
+    </div>
+      <?php  } ?>
+   
+   
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="exampleInputEmail1" class="form-label">user name</label>
+    <input type="text" name="username" class="form-control" >
    
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
   
   <button type="submit" class="btn btn-primary">login</button>
