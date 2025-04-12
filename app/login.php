dc233b7c2720ddfaf36f830ff8ae7c53928d1464
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-13 01:17:02 +0530

    update code

--- app/login.php
+++ app/login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "../Db_Connection.php";
 if(isset($_POST['username']) && isset($_POST['password'])){
     function validate_input($data){
@@ -22,13 +23,42 @@ if(isset($_POST['username']) && isset($_POST['password'])){
     }else{
         $sql = "SELECT * FROM user WHERE username = ?";
         $stmt = $conn->prepare($sql);
-        $stmt = execute($username);
+        $stmt -> execute([$username]);
 
         if($stmt -> rowCount() == 1){
             $user = $stmt->fetch();
             $usernameOb = $user['user_name'];
             $passwordOb = $user['password'];
             $role = $user['role'];
+            $id = $user['id'];
+
+            if($user_name === $usernameOb){
+                 if(password_verify($password,$passwordOb)){
+                     if($role == "admin"){
+                            $_SESSION['role'] = $role;
+                            $_SESSION['id'] = $id;
+                            $_SESSION['username'] = $usernameOb;
+                            header("location:../index.php");
+                     }else if($role == "employee"){
+                        $_SESSION['role'] = $role;
+                        $_SESSION['id'] = $id;
+                        $_SESSION['username'] = $usernameOb;
+                        header("location:../index.php");
+                     }else{
+                        $em = "unkown error occurred";
+                        header("location:../login.php?error=$em");
+                        exit(); 
+                     }
+                 }else{
+                    $em = "incorrect username or password";
+                    header("location:../login.php?error=$em");
+                    exit(); 
+                 }
+            }else{
+                $em = "incorrect username or password";
+                header("location:../login.php?error=$em");
+                exit(); 
+            }
         }
     }
 
