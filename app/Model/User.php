42521f08ee6472072fc7cad87e039db375976926
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-13 21:28:04 +0530

    update

---
+++ app/Model/User.php
@@ -0,0 +1,20 @@
+<?php
+
+function get_all_users($conn){
+    $sql = "SELECT * FROM user WHERE role = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(["employee"]);
+
+    if($stmt -> rowCount() > 0){
+        $users = $stmt->fetchAll();
+    }else $users = 0;
+
+    return $users;
+}
+
+
+
+
+
+
+?>
